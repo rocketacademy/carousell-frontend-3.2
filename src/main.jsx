8f4674b5eff24aab88342ea474279b7ee76e87fe
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-11-10 17:35:53 +0800

    solution-auth-base

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 import {
   createBrowserRouter,
@@ -22,7 +23,7 @@ const router = createBrowserRouter(
 
       <Route path="new" element={<NewListingForm />} />
       {/* Route that renders individual sightings */}
-      <Route path="sightings/:sightingId" element={<Listing />} />
+      <Route path="listings/:listingId" element={<Listing />} />
       {/* Route that matches all other paths */}
       <Route path="*" element={"Nothing here!"} />
     </Route>
@@ -30,5 +31,16 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <Auth0Provider
+    domain="dev-0qzz65qu6ebsz52w.au.auth0.com"
+    clientId="RTCH8xyl1CRiB45nAXyoEeHSwL7LacWv"
+    authorizationParams={{
+      redirect_uri: "http://localhost:5173",
+      audience: "https://carousel/api",
+      scope:
+        "read:current_user update:current_user_metadata openid profile email",
+    }}
+  >
+    <RouterProvider router={router} />
+  </Auth0Provider>
 );
