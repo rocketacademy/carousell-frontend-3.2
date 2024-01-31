b36edd76b6013af59269c8f562f1ba7248d784d1
estney <esther.pxy@gmail.com>
2024-01-31 19:32:52 +0800

    completed listing

--- src/Components/Listing.jsx
+++ src/Components/Listing.jsx
@@ -5,10 +5,14 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 import { BACKEND_URL } from "../../constants.jsx";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Listing = () => {
   const [listingId, setListingId] = useState();
   const [listing, setListing] = useState({});
+
+  const { isAuthenticated, loginWithRedirect, getAccessTokenSilently, user } =
+    useAuth0();
 
   useEffect(() => {
     // If there is a listingId, retrieve the listing data
@@ -36,10 +40,35 @@ const Listing = () => {
     }
   }
 
-  const handleClick = () => {
-    axios.put(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
-      setListing(response.data);
+  const handleClick = async () => {
+    //if user is not authenticated, redirect to sign in
+    if (!isAuthenticated) {
+      loginWithRedirect();
+      return;
+    }
+    //get access token
+
+    const accessToken = await getAccessTokenSilently({
+      audience: import.meta.env.VITE_SOME_AUTH0_AUDIENCE,
+      scope: "read:current_user",
     });
+
+    console.log(accessToken);
+
+    //edit listing
+    const response = await axios.put(
+      `${BACKEND_URL}/listings/${listingId}`,
+      // User is currently logged-in user
+      { email: user.email },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    // Update the listing on the page
+    setListing(response.data);
   };
 
   return (
