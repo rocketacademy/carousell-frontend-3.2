6e8196f8d71ede19e9be5bbe6a4d8783699b4b3a
Chxshire22 <akuindra22@gmail.com>
2024-01-31 13:47:20 +0800

    added auth0

--- src/Components/Listing.jsx
+++ src/Components/Listing.jsx
@@ -3,59 +3,87 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-
+import { useAuth0 } from "@auth0/auth0-react";
 import { BACKEND_URL } from "../../constants.jsx";
 
 const Listing = () => {
-  const [listingId, setListingId] = useState();
-  const [listing, setListing] = useState({});
+	const [listingId, setListingId] = useState();
+	const [listing, setListing] = useState({});
+	const [accessToken, setAccessToken] = useState("");
 
-  useEffect(() => {
-    // If there is a listingId, retrieve the listing data
-    if (listingId) {
-      axios.get(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
-        setListing(response.data);
-      });
-    }
-    // Only run this effect on change to listingId
-  }, [listingId]);
+	const {
+		user,
+		isAuthenticated,
+		loginWithRedirect,
+		getAccessTokenSilently,
+		logout,
+	} = useAuth0();
 
-  // Update listing ID in state if needed to trigger data retrieval
-  const params = useParams();
-  if (listingId !== params.listingId) {
-    setListingId(params.listingId);
-  }
+	const checkUser = async () => {
+		if (isAuthenticated) {
+			let token = await getAccessTokenSilently();
+			console.log("token", token);
+			setAccessToken(token);
+		} else {
+			loginWithRedirect();
+		}
+	};
+	useEffect(() => {
+		// If there is a listingId, retrieve the listing data
+		if (listingId) {
+			axios.get(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
+				setListing(response.data);
+			});
+		}
+		// Only run this effect on change to listingId
+	}, [listingId]);
 
-  // Store a new JSX element for each property in listing details
-  const listingDetails = [];
-  if (listing) {
-    for (const key in listing) {
-      listingDetails.push(
-        <Card.Text key={key}>{`${key}: ${listing[key]}`}</Card.Text>
-      );
-    }
-  }
+	// Update listing ID in state if needed to trigger data retrieval
+	const params = useParams();
+	if (listingId !== params.listingId) {
+		setListingId(params.listingId);
+	}
 
-  const handleClick = () => {
-    axios.put(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
-      setListing(response.data);
-    });
-  };
+	// Store a new JSX element for each property in listing details
+	const listingDetails = [];
+	if (listing) {
+		for (const key in listing) {
+			listingDetails.push(
+				<Card.Text key={key}>{`${key}: ${listing[key]}`}</Card.Text>
+			);
+		}
+	}
 
-  return (
-    <div>
-      <Link to="/">Home</Link>
-      <Card bg="light">
-        <Card.Body>
-          {listingDetails}
-          <Button onClick={handleClick} disabled={listing.BuyerId}>
-            Buy
-          </Button>
-        </Card.Body>
-      </Card>
-      <br />
-    </div>
-  );
+	const handleClick = () => {
+		checkUser();
+		axios
+			.put(`${BACKEND_URL}/listings/${listingId}`, {
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			})
+			.then((response) => {
+				setListing(response.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<div>
+			<Link to="/">Home</Link>
+			<Card bg="light">
+				<Card.Body>
+					{listingDetails}
+					<Button onClick={handleClick} disabled={listing.BuyerId}>
+						Buy
+					</Button>
+				</Card.Body>
+			</Card>
+			<br />
+		</div>
+	);
 };
 
 export default Listing;
--- src/main.jsx
+++ src/main.jsx
@@ -4,31 +4,44 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
 } from "react-router-dom";
 
 import Home from "./Components/Home";
 import NewListingForm from "./Components/NewListingForm";
 import Listing from "./Components/Listing";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      {/* Route that renders home content */}
-      <Route index element={<Home />} />
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			{/* Route that renders home content */}
+			<Route index element={<Home />} />
 
-      <Route path="new" element={<NewListingForm />} />
-      {/* Route that renders individual sightings */}
-      <Route path="sightings/:sightingId" element={<Listing />} />
-      {/* Route that matches all other paths */}
-      <Route path="*" element={"Nothing here!"} />
-    </Route>
-  )
+			<Route path="/listings/new" element={<NewListingForm />} />
+			{/* Route that renders individual sightings */}
+			<Route path="listings/:listingId" element={<Listing />} />
+			{/* Route that matches all other paths */}
+			<Route path="*" element={"Nothing here!"} />
+		</Route>
+	)
 );
 
+// localhost:5173/new
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+	<Auth0Provider
+		domain={ import.meta.env.VITE_APP_DOMAIN }
+		clientId={ import.meta.env.VITE_APP_CLIENDID }
+		authorizationParams={{
+			redirect_uri: window.location.origin,
+			audience:import.meta.env.VITE_APP_AUDIENCE,
+			scope: "read:current_user update:current_user_metadata openid profile email",
+		}}
+	>
+		<RouterProvider router={router} />
+	</Auth0Provider>
 );
